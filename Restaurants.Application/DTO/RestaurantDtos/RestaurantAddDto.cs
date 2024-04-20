14d4987a1a0aed0f72bf0302f0725b44b046ec0d
Artem Sorokin <karanthir8@gmail.com>
2024-04-20 08:59:56 +0500

    configure and register fluentValidation

--- Restaurants.Application/DTO/RestaurantDtos/RestaurantAddDto.cs
+++ Restaurants.Application/DTO/RestaurantDtos/RestaurantAddDto.cs
@@ -8,10 +8,8 @@ public class RestaurantAddDto
     public string Description { get; set; } = default!;
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
-
     public string? ContactEmail { get; set; }
     public string? ContactNumber { get; set; }
-
     public string? City { get; set; }
     public string? Street { get; set; }
     public string? PostalCode { get; set; }
--- Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Application.ServiceContracts;
 using Restaurants.Application.Services;
@@ -9,6 +11,12 @@ public static class ServiceCollectionExtensions
     public static void AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IRestaurantsService, RestaurantsService>();
-        services.AddAutoMapper(typeof(ServiceCollectionExtensions).Assembly);
+        
+        var appAssembly = typeof(ServiceCollectionExtensions).Assembly;
+        
+        services.AddAutoMapper(appAssembly);
+
+        services.AddValidatorsFromAssembly(appAssembly)
+            .AddFluentValidationAutoValidation();
     }
 }
