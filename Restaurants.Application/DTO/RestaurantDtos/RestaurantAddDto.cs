69173a7f5d637a4c301ace3d0220e0ea5d2a498f
Artem Sorokin <karanthir8@gmail.com>
2024-04-20 08:56:29 +0500

    add FluentValidation -> addDtoValidator class

--- Restaurants.Application/DTO/RestaurantDtos/RestaurantAddDto.cs
+++ Restaurants.Application/DTO/RestaurantDtos/RestaurantAddDto.cs
@@ -4,25 +4,15 @@ namespace Restaurants.Application.DTO.RestaurantDtos;
 
 public class RestaurantAddDto
 {
-    [StringLength(maximumLength:100, MinimumLength = 3)]
     public string Name { get; set; } = default!;
-    
-    [StringLength(maximumLength: 500, MinimumLength = 3)]
     public string Description { get; set; } = default!;
-    
-    [Required(ErrorMessage = "Category is required")]
     public string Category { get; set; } = default!;
     public bool HasDelivery { get; set; }
 
-    [EmailAddress(ErrorMessage = "Invalid email address")]
     public string? ContactEmail { get; set; }
-    
-    [Phone(ErrorMessage = "Invalid phone number")]
     public string? ContactNumber { get; set; }
 
     public string? City { get; set; }
     public string? Street { get; set; }
-    
-    [RegularExpression(@"^[0-9]{6}$", ErrorMessage = "Invalid postal code, valid example: 123456")]
     public string? PostalCode { get; set; }
 }
---
+++ Restaurants.Application/Validators/RestaurantAddDtoValidator.cs
@@ -0,0 +1,29 @@
+using FluentValidation;
+using Restaurants.Application.DTO.RestaurantDtos;
+
+namespace Restaurants.Application.Validators;
+
+public class RestaurantAddDtoValidator : AbstractValidator<RestaurantAddDto>
+{
+    public RestaurantAddDtoValidator()
+    {
+        RuleFor(dto => dto.Name)
+            .Length(3, 100);
+
+        RuleFor(dto => dto.Description)
+            .NotEmpty().WithMessage("Description is required");
+        
+        RuleFor(dto => dto.Category)
+            .NotEmpty().WithMessage("Category is required");
+        
+        RuleFor(dto => dto.ContactEmail)
+            .EmailAddress().WithMessage("Invalid email address");
+        
+        RuleFor(dto => dto.ContactNumber)
+            .Matches(@"^\d{10}$")
+            .WithMessage("Phone number must be 10 digits");
+        
+        RuleFor(dto => dto.PostalCode)
+            .Matches(@"^[0-9]{6}$").WithMessage("Postal code is required in 6 digits format");
+    }
+}
