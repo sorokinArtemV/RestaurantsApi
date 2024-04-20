2cd7f51085fdcf4c988e4d100ab0c76606931173
Artem Sorokin <karanthir8@gmail.com>
2024-04-20 06:51:46 +0500

    add dto to services

--- Restaurants.Application/Services/RestaurantsService.cs
+++ Restaurants.Application/Services/RestaurantsService.cs
@@ -1,26 +1,35 @@
+using AutoMapper;
 using Microsoft.Extensions.Logging;
 using Restaurants.Application.DTO;
 using Restaurants.Application.ServiceContracts;
-using Restaurants.Core.Entities;
 using Restaurants.Core.RepositoryContracts;
 
 namespace Restaurants.Application.Services;
 
 public class RestaurantsService(
-    IRestaurantsRepository repository, ILogger<RestaurantsService> logger) : IRestaurantsService
+    IRestaurantsRepository repository,
+    ILogger<RestaurantsService> logger,
+    IMapper mapper
+) : IRestaurantsService
 {
     public async Task<IEnumerable<RestaurantDto>> GetAllRestaurants()
     {
         logger.LogInformation("Getting all restaurants");
         var restaurants = await repository.GetAllAsync();
-        
-        return restaurants;
+
+        var restaurantsDto = mapper.Map<IEnumerable<RestaurantDto>>(restaurants);
+
+        return restaurantsDto;
     }
 
     public async Task<RestaurantDto?> GetRestaurantById(int id)
     {
         logger.LogInformation("Getting restaurant by id: {Id}", id);
-        
-        return await repository.GetByIdAsync(id);
+
+        var restaurant = await repository.GetByIdAsync(id);
+
+        var restaurantDto = mapper.Map<RestaurantDto>(restaurant);
+
+        return restaurantDto;
     }
 }
