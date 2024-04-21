44417056054ee1a85d384164a3d9d4b00231be23
Artem Sorokin <karanthir8@gmail.com>
2024-04-21 15:59:24 +0500

    move serilog config to appsettings dev

--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -12,13 +12,10 @@ builder.Services.AddControllers();
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Host.UseSerilog(((context, configuration) =>
+builder.Host.UseSerilog((context, configuration) =>
 {
-    configuration
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-        .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-        .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}");
-}));
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 var app = builder.Build();
 
