62e044e460eb8a5190d4a6660fec4ba49f051f5d
Artem Sorokin <karanthir8@gmail.com>
2024-04-22 07:36:21 +0500

    add post addDish to webapi client

--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -20,6 +20,7 @@ public static class ServiceCollectionExtensions
         
         services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
         services.AddScoped<IRestaurantsRepository, RestaurantsRepository>();
+        services.AddScoped<IDishesRepository, DishesRepository>();
     }
     
     public static async Task InitializeDatabaseAsync(this IServiceProvider serviceProvider)
