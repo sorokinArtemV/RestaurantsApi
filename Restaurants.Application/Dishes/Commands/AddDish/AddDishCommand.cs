06069491ca62de3671cee31a3ceae0352e8fc509
Artem Sorokin <karanthir8@gmail.com>
2024-04-22 06:59:56 +0500

    add AddDishCommand

--- Restaurants.Application/Dishes/Commands/AddDish/AddDishCommand.cs
+++ Restaurants.Application/Dishes/Commands/AddDish/AddDishCommand.cs
@@ -1,6 +1,12 @@
+using MediatR;
+
 namespace Restaurants.Application.Dishes.Commands.AddDish;
 
-public class AddDishCommand
+public class AddDishCommand : IRequest
 {
-    `
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public decimal Price { get; set; }
+
+    public int? KiloCalories { get; set; }
 }
