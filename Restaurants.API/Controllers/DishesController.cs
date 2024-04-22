775e9f4de414c681013855597aa16231b40aaec0
Artem Sorokin <karanthir8@gmail.com>
2024-04-23 04:30:16 +0500

    add GetAllDishesForRestaurant method

--- Restaurants.API/Controllers/DishesController.cs
+++ Restaurants.API/Controllers/DishesController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Restaurants.Application.Dishes.Commands.AddDish;
+using Restaurants.Application.Dishes.DTO;
 
 namespace Restaurants.API.Controllers;
 
@@ -12,9 +13,17 @@ public class DishesController(IMediator mediator) : ControllerBase
     public async Task<IActionResult> AddDish(int restaurantId, AddDishCommand command)
     {
         command.RestaurantId = restaurantId;
-        
+
         await mediator.Send(command);
 
         return Created();
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<DishDto>> GetAllDishesForRestaurant(int restaurantId)
+    {
+        var dishes = await mediator.GetDishesForRestaurantQuery(restaurantId);
+
+        return Ok(dishes);
     }
 }
