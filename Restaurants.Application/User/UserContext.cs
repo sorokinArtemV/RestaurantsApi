288d17677c2292868003b5c1f71c526fcea153e4
Artem Sorokin <karanthir8@gmail.com>
2024-04-24 05:20:10 +0500

    add UserContext class

--- Restaurants.Application/User/UserContext.cs
+++ Restaurants.Application/User/UserContext.cs
@@ -1,6 +1,22 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
 namespace Restaurants.Application.User;
 
-public class UserContext
+public class UserContext(IHttpContextAccessor contextAccessor)
 {
-    
+    public CurrentUser? GetCurrentUser()
+    {
+        var user = contextAccessor.HttpContext?.User;
+
+        if (user == null) throw new InvalidOperationException("User context is not present");
+
+        if (user.Identity is not { IsAuthenticated: true }) return null;
+
+        var userId = user.FindFirst(x => x.Type == ClaimTypes.NameIdentifier)!.Value;
+        var email = user.FindFirst(x => x.Type == ClaimTypes.Email)!.Value;
+        var roles = user.Claims.Where(x => x.Type == ClaimTypes.Role).Select(x => x.Value);
+        
+        return new CurrentUser(userId, email, roles);
+    }
 }
