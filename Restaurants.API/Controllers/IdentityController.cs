0071b7eaa6e673a9f3de6e5be905baefc90f3675
Artem Sorokin <karanthir8@gmail.com>
2024-04-24 06:02:01 +0500

    add UpdateUserMethod to constructor

---
+++ Restaurants.API/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Users.Commands;
+
+namespace Restaurants.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class IdentityController(IMediator mediator) : ControllerBase
+{
+    [HttpPatch("user")]
+    [Authorize]
+    public async Task<IActionResult> UpdateUser(UpdateUserCommand command)
+    {
+        await mediator.Send(command);
+        
+        return NoContent();
+    }
+}
---
+++ Restaurants.Infrastructure/Migrations/20240424003308_AddAdditionalUserProps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurants.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdditionalUserProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Restaurants",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nationality",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nationality",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Restaurants",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
