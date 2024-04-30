bf9c79fbd4bf4713e842744d058ef0ec55f45028
Artem Sorokin <karanthir8@gmail.com>
2024-04-30 05:53:31 +0500

    extend unit tests for UserContextTests - GetCurrentUser method

--- z_Restaurants.ApplicationTests/Users/UserContextTests.cs
+++ z_Restaurants.ApplicationTests/Users/UserContextTests.cs
@@ -44,4 +44,17 @@ public class UserContextTests
         currentUser.Nationality.Should().Be("Japanese");
         currentUser.DateOfBirth.Should().Be(dateOfBirth);
     }
+
+    [Fact]
+    public void GetCurrentUser_ShouldThrowInvalidOperationException_WhenUserContextIsNull()
+    {
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAccessorMock.Setup(x => x.HttpContext).Returns((HttpContext)null);
+
+        var userContext = new UserContext(httpContextAccessorMock.Object);
+
+        var action = () => userContext.GetCurrentUser();
+        
+        action.Should().Throw<InvalidOperationException>().WithMessage("Users context is not present");
+    }
 }
